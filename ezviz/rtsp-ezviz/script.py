fb3b906a07e62fbf7cd3f4436a8eeebe8c8d7a00
crabtree <3641496+crabtree@users.noreply.github.com>
2024-02-11 22:16:01 +0100

    Handle ezviz ip address in script.py (#21)

--- ezviz/rtsp-ezviz/script.py
+++ ezviz/rtsp-ezviz/script.py
@@ -6,6 +6,7 @@ import subprocess
 import os 
 
 pwd = os.getenv("EZVIZ_PASS", "")
+addr = os.getenv("EZVIZ_ADDR", "")
 
 dir = "/output"
 if not os.path.exists(f"{dir}"):
@@ -17,5 +18,5 @@ while True:
   name = f"/{now.year}{now.month:02d}{now.day:02d}{now.hour:02d}{now.minute:02d}{now.second:02d}"
   if not os.path.exists(f"{dir}{subdir}"):
     os.makedirs(f"{dir}{subdir}")
-  subprocess.run(["ffmpeg", "-rtsp_transport", "tcp", "-i", f"rtsp://admin:{pwd}@192.168.125.23/h264_stream", "-f", "image2", "-vframes", "1", "-pix_fmt", "yuvj420p", f"{dir}{subdir}{name}.jpg"])
+  subprocess.run(["ffmpeg", "-rtsp_transport", "tcp", "-i", f"rtsp://admin:{pwd}@{addr}/h264_stream", "-f", "image2", "-vframes", "1", "-pix_fmt", "yuvj420p", f"{dir}{subdir}{name}.jpg"])
   time.sleep(55)
